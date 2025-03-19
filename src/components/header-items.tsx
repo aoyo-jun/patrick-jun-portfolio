118e776fbac3a6b77c39e3f38d0717109897bf53
akaruijun <patrickjun2208@gmail.com>
2025-03-19 19:44:41 -0300

    Add mobile first and fix LinkedIn letter spacing

--- src/components/header-items.tsx
+++ src/components/header-items.tsx
@@ -2,16 +2,23 @@ interface HeaderItemsProps {
     mainText: string;
     shortcut: string;
     href: string;
+    isLinkedin?: boolean;
 }
 
-export default function HeaderItems({mainText, shortcut, href}: HeaderItemsProps) {
+export default function HeaderItems({mainText, shortcut, href, isLinkedin}: HeaderItemsProps) {
     return (
         <a href={href}>
-            <div className="flex items-center justify-center bg-default-primary hover:bg-default-secondary h-[27px] px-[9px] pt-[9px] pb-[11px] rounded-[3px] tracking-[-0.02em]">
-                <span className="font-koho mr-[6px]">
+            <div className="flex items-center justify-center bg-default-primary hover:bg-default-secondary h-[24px] lg:h-[27px] px-[7px] lg:px-[9px] pt-[9px] pb-[9px] lg:pb-[11px] rounded-[3px] tracking-[-0.02em]">
+                <span className="font-koho mr-[6px] hidden lg:flex">
                     [<span className="font-kodemono">{shortcut}</span>]
                 </span>
-                {mainText}
+                {/* LinkedIn letter spacing fix */}
+                {isLinkedin ? (
+                    <>
+                        <span className="mr-[1px]">LIN</span>
+                        KEDIN
+                    </>
+                ) : (`${mainText}`)}
             </div>
         </a>
     )
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,12 +2,12 @@ import HeaderItems from "./header-items"
 
 export default function Header() {
     return (
-        <header className="opacity-100 translate-none rotate-none scale-none transform-none font-kodemono text-[0.9em] my-[32px]">
+        <header className="opacity-100 translate-none rotate-none scale-none transform-none font-kodemono text-[0.725em] lg:text-[0.9em] mx-[12px] my-[12px] lg:mx-0 lg:my-[32px]">
             {/* Outer Div */}
-            <div className="flex flex-row items-center justify-center gap-[3px]">
+            <div className="flex flex-row items-center justify-start lg:justify-center gap-[2px] lg:gap-[3px]">
                 {/* Home */}
                 <a href="/">
-                    <div className="flex items-center justify-center bg-default-primary hover:bg-default-secondary size-[27px] pt-[9px] pb-[11px] rounded-[3px]">
+                    <div className="flex items-center justify-center bg-default-primary hover:bg-default-secondary size-[24px] lg:size-[27px] pt-[9px] pb-[11px] rounded-[3px]">
                         /
                     </div>
                 </a>
@@ -15,7 +15,7 @@ export default function Header() {
                 <HeaderItems mainText={"ABOUT"} shortcut={"A"} href="#about" />
                 <HeaderItems mainText={"PROJECTS"} shortcut={"P"} href="#projects" />
                 <HeaderItems mainText={"GITHUB"} shortcut={"G"} href="https://github.com/aoyo-jun" />
-                <HeaderItems mainText={"LINKEDIN"} shortcut={"L"} href="https://www.linkedin.com/in/patrick-jun-miyaura-ihy" />
+                <HeaderItems mainText={"LINKEDIN"} shortcut={"L"} isLinkedin href="https://www.linkedin.com/in/patrick-jun-miyaura-ihy" />
                 <HeaderItems mainText={"CONTACT"} shortcut={"C"} href="#contact" />
             </div>
         </header>
